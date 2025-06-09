14936b8a214c3eab2f0eaa79e0e1fb7bb309e5f9
GitMicaFri <micaela.fristedt@gmail.com>
2025-06-09 10:38:38 +0200

    Add more pages to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
 import LandingPage from './Pages/Landing/landing'
-import CoffeeMenuPage from './Pages/CoffeeMenu/coffeeMenu';
+import CoffeeMenuPage from './Pages/CoffeeMenu/coffeeMenu'
+import OrderHistoryPage from './Pages/OrderHistory/history'
+import PopUpMenuPage from './Pages/PopUpMenu/popUpMenu'
+import ProfilePage from './Pages/Profile/profile'
+import StatusPage from './Pages/Profile/profile'
 
 function App() {
   return (
@@ -10,7 +14,11 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} /> 
-        <Route path="/coffee-menu" element={<CoffeeMenuPage />} />  
+        <Route path="/coffee-menu" element={<CoffeeMenuPage />} />
+        <Route path="/coffee-menu" element={<OrderHistoryPage />} /> 
+        <Route path="/coffee-menu" element={<PopUpMenuPage />} /> 
+        <Route path="/coffee-menu" element={<ProfilePage />} /> 
+        <Route path="/coffee-menu" element={<StatusPage />} />   
       </Routes>
     </Router>
   )
